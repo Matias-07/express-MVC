79493f82b975cfdeb6c7fe9b2f079d59176c96b5
Matias-07 <matias.ferrari.m@gmail.com>
2024-08-06 23:40:06 -0300

    search ready!

--- src/controllers/listController.js
+++ src/controllers/listController.js
@@ -9,9 +9,27 @@ let listControllers = {
                 ]
                 res.render('userList', { 'users': users })
         },
-        'search': function(req, res) {
-                let usersSearch = req.query;
-                res.send(usersSearch)
+        'search': function (req, res) {
+                let usersSearch = req.query.search;
+
+                let users = [{ id: 1, name: 'Dario' },
+                { id: 2, name: 'Javier' },
+                { id: 3, name: 'Matias' },
+                { id: 4, name: 'Agustin' },
+                { id: 5, name: 'silvana' },
+                { id: 6, name: 'gabriel' }
+                ];
+
+                let usersResults = []; 
+
+                for (let i = 0; i < users.length; i++) {
+                        if (users[i].name.includes(usersSearch)) {
+                                usersResults.push(users[i]); 
+                        }
+                }
+
+                res.render('userResults', {usersResults: usersResults})
+
         }
 }
 
