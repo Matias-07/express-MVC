99ffff1bc766b1da4486787da12b7d44550bd8b8
Matias-07 <matias.ferrari.m@gmail.com>
2024-08-12 15:37:10 -0300

    uploading files to server

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,21 +1,36 @@
-let express = require('express'); 
-let listControllers = require ('../controllers/listController.js')
+let express = require('express');
+let listControllers = require('../controllers/listController.js');
+const multer = require('multer');
+const path = require('path');
 
-let router = express.Router(); 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../../public/images/profileImages'))
+    },
+    filename: (req, file, cb) => {
+        console.log(file);
+        const newFileName = 'profile-' + Date.now() + path.extname(file.originalname)
+        cb(null, newFileName)
+    }
+});
 
-router.get('/', function (req,res) {
-    res.render('index'); 
-}); 
+const upload = multer({ storage });
 
-router.get('/contacto', function(req,res) {
+let router = express.Router();
+
+router.get('/', function (req, res) {
+    res.render('index');
+});
+
+router.get('/contacto', function (req, res) {
     res.send('Dejanos tu contacto')
 });
 
-router.get('/register', function (req,res) {
-    res.render('register'); 
-}); 
+router.get('/register', function (req, res) {
+    res.render('register');
+});
 
-router.post('/register', listControllers.create)
+router.post('/register', upload.single('imagenUsuario'), listControllers.create)
 
 router.get('/list', listControllers.list)
 
@@ -23,11 +38,11 @@ router.get('/search', listControllers.search)
 
 router.get('/edit/:idUser', listControllers.edit)
 
-router.put('/edit', function(req,res){
+router.put('/edit', function (req, res) {
     res.send('Estoy en PUT!');
 });
 
-router.delete('/delete/:idUser', function(req,res){
+router.delete('/delete/:idUser', function (req, res) {
     res.send('Viajo por Delete');
 })
 
